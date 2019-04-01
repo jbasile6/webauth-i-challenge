650961bad44b3438469dbb1058ee39db578953d3
James Basile <jbasile145@gmail.com>
2019-04-01 19:57:46 -0400

    POST endpoint complete, GET endpoint complete, not sure how to do session/cookie problem

--- index.js
+++ index.js
@@ -32,7 +32,28 @@ server.post('/api/register', (req, res) => {
 });
 
 //get all users
-server.get('/api/users', )
+server.get('/api/users', (req, res) => {
+    db('users')
+        .then(user => res.status(200).json(user))
+        .catch(err => res.status(500).json(err));
+})
+
+
+server.post('/api/login', (req, res) => {
+    db('users')
+        .where({ username: req.body.username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(req.body.password, user.password)) {
+                res.status(200).json({ message: `Welcome ${user.username}`, cookie: user.id })
+                //not sure how to create a new session, based on what i'm seeeing in tomorrow's TK, the cookie isnt correct either
+                //looks like this is for tomorrow's project
+            } else{
+                res.status(400).json({ error: 'You shall not pass' })
+            }
+        })
+        .catch(err => res.status(500).json(err));
+})
 
 server.listen(5000, () => {
     console.log(`\n** Running on port 5k **\n`)
