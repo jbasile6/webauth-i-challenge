1328230fc56753654479ecb915a85d60381b39c3
James Basile <jbasile145@gmail.com>
2019-04-01 19:10:53 -0400

    created new database

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ server.get('/', (req, res) => {
 })
 
 
+//POST create new user
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+    db('users').insert(user)
+        .then(newUser => res.status(201).json(newUser))
+        .catch( err => res.status(500).json(err));
+});
+
+//get all users
+server.get('/api/users', )
 
 server.listen(5000, () => {
     console.log(`\n** Running on port 5k **\n`)
